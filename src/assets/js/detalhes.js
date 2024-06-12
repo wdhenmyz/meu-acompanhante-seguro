8558b68c01754228f764ae3e49b2ab26a877bdfb
Gelson Lima de Souza Junior <153616550+gelsonli@users.noreply.github.com>
2024-06-12 11:40:28 -0300

    html e css

--- src/assets/js/detalhes.js
+++ src/assets/js/detalhes.js
@@ -1,10 +1,4 @@
-let body = document.querySelector('body')
 
-// função para modo dark
-trilho.addEventListener('click', () => {
-    trilho.classList.toggle('dark')
-    body.classList.toggle('dark')
-})
 
 
 
@@ -38,3 +32,69 @@ function aumentarTamanho() {
       }
     }
   }
+
+
+  let isReading = false;
+// Seleciona todos os botões de leitura
+const botoesLer = document.querySelectorAll('.ler-button');
+
+// Adiciona evento de clique a cada botão
+botoesLer.forEach(botao => {
+    botao.addEventListener('click', () => {
+        if (isReading) {
+            // Para a leitura atual se já estiver lendo
+            speechSynthesis.cancel();
+            botao.textContent = 'Ler em Voz Alta';
+            isReading = false;
+        } else {
+            // Obtém o texto do elemento pai do botão
+            const texto = botao.parentElement.textContent.replace('Ler em Voz Alta', '').trim();
+            // Cria objeto SpeechSynthesisUtterance
+            const utterance = new SpeechSynthesisUtterance(texto);
+            // Adiciona evento para detectar quando a leitura termina
+            utterance.onend = () => {
+                isReading = false;
+                botao.textContent = 'Ler em Voz Alta';
+            };
+            // Inicia a leitura do texto
+            speechSynthesis.speak(utterance);
+            botao.textContent = 'Parar Leitura';
+            isReading = true;
+        }
+    });
+});
+
+
+
+//   let isReading = false;
+// // Seleciona todos os botões de leitura
+// const botoesLer = document.querySelectorAll('.ler-button');
+
+// // Adiciona evento de clique a cada botão
+// botoesLer.forEach(botao => {
+//     botao.addEventListener('click', () => {
+//         console.log("clicou");
+//         if (isReading) {
+//             console.log("clicou");
+//             // Para a leitura atual se já estiver lendo
+//             speechSynthesis.cancel();
+//             botao.textContent = 'Ler em Voz Alta';
+//             isReading = false;
+//         } else {
+//             console.log("clicou");
+//             // Obtém o texto do elemento pai do botão
+//             const texto = botao.parentElement.textContent.replace('Ler em Voz Alta', '').trim();
+//             // Cria objeto SpeechSynthesisUtterance
+//             const utterance = new SpeechSynthesisUtterance(texto);
+//             // Adiciona evento para detectar quando a leitura termina
+//             utterance.onend = () => {
+//                 isReading = false;
+//                 botao.textContent = 'Ler em Voz Alta';
+//             };
+//             // Inicia a leitura do texto
+//             speechSynthesis.speak(utterance);
+//             botao.textContent = 'Parar Leitura';
+//             isReading = true;
+//         }
+//     });
+// });
